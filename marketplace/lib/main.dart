03671d49780ed88a798952e397853b66f5c31abd
Alex Calcan <79245328+atcalcan@users.noreply.github.com>
2022-04-03 12:44:46 +0300

    marketplace obiect separat

---
+++ marketplace/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:marketplace/marketplace.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  HomePage();
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Marketplace"),
+      ),
+      body: Center(
+        child: Marketplace(),
+      ),
+    );
+  }
+}
---
+++ marketplace/lib/marketplace.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class Marketplace extends StatefulWidget {
+  Marketplace();
+
+  @override
+  State<Marketplace> createState() => _MarketplaceState();
+}
+
+class _MarketplaceState extends State<Marketplace> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
