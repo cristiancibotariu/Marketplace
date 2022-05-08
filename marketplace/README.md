8c0512ac6e82a7d6f7445cf5899f50c623871b09
cristiancibotariu <cristian.cibotariu2@gmail.com>
2022-05-08 11:54:07 +0300

    actualizat ca sa fim la zi cu repo-ul mare

--- marketplace/README.md
+++ marketplace/README.md
@@ -1,4 +1,4 @@
-# marketplace
+# frontend
 
 A new Flutter project.
 
--- marketplace/test/widget_test.dart
+++ marketplace/test/widget_test.dart
@@ -7,9 +7,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:homepage/main.dart';
 
-// import 'package:marketplace/main.dart';
+import 'package:frontend/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
